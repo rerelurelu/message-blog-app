dff7303ccd88e3014a36f9f2995fe3b08b379c78
zoniha <awaawapurin@gmail.com>
2021-07-16 11:25:36 +0900

    🎉 init: Initial commit

---
+++ android/app/src/main/kotlin/com/zaw/message_blog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zaw.message_blog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import './views/home_view.dart';
+
+void main() {
+  runApp(MessageBlogApp());
+}
+
+class MessageBlogApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Message Blog',
+      theme: ThemeData.dark(),
+      home: HomeView(),
+    );
+  }
+}
